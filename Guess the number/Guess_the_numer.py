cb6a03602e34018fd30c103c304b953598123f0c
Mateusz <mt.fret@gmail.com>
2023-01-14 14:18:45 +0100

    First Commit

---
+++ Guess the number/Guess_the_numer.py
@@ -0,0 +1,26 @@
+import random
+
+random_number = random.randint(1,100)
+
+def guess_the_number():
+
+    correct = False
+    while not correct:
+        n = input("Guess the number: ")
+        try:
+            n = int(n)
+        except ValueError:
+            print("It's not a number")
+            continue
+        if n < random_number:
+            print("Too small!")
+            continue
+        elif n > random_number:
+            print("Too big!")
+            continue
+        else:
+            print("You win!")
+            correct = True
+
+
+guess_the_number()
